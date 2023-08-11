2531c3899dfc455e407abf451c785e2263933f4d
Oluwabunmi Akapo <107144014+B-Akapo@users.noreply.github.com>
2023-08-12 00:10:32 +0100

    modied 24-trianglearea.py and 25-gcd.py with docstrings

--- python-basic-1/24-trianglearea.py
+++ python-basic-1/24-trianglearea.py
@@ -1,5 +1,16 @@
 # Description: A program that will accept the base and height of a triangle and compute its area. 
 
+"""
+
+Explanation:
+- The program prompts the user to enter the values for the base and height of the triangle. 
+- It then validates the input to ensure that both values are positive integers. 
+- If the input is valid, it calculates the area of the triangle using the formula 0.5 * base * height and prints the result. 
+- If the input is invalid, it displays an error message.
+- Added exception handling to catch invalid input values and display appropriate error messages.
+- Separated the input validation and area calculation into separate functions for better code organization.
+"""
+
 def main():
     try:
         base = int(input("Please enter the value of the base: "))
--- python-basic-1/25-gcd.py
+++ python-basic-1/25-gcd.py
@@ -1,7 +1,21 @@
 # Description: A program that computes the greatest common divisor (GCD) of two positive integers. 
 
-import math  
+"""
+This code snippet calculates the greatest common divisor (GCD) of two positive integers.
+
+
+Explanation:
+- The code snippet prompts the user to enter two positive integers and calculates their GCD using the math.gcd() function. 
+- The result is then printed.
+- There is an input validation to ensure that the user enters positive integers.
+"""
+
+import math 
+
 def main():
+    """
+    Entry point of the program.
+    """
     num1 = int(input("Enter the first positive integer: "))
     num2 = int(input("Enter the second positive integer: "))
     
@@ -9,6 +23,16 @@ def main():
     print(f"The GCD of {num1} and {num2} is {gcd_result}")
 
 def calculate_gcd(a, b):
+    """
+    Calculates the GCD of two numbers using the math.gcd() function.
+    
+    Args:
+        a: First positive integer.
+        b: Second positive integer.
+    
+    Returns:
+        The GCD of the two numbers.
+    """
     return math.gcd(a, b)
 
 if __name__ == "__main__":
