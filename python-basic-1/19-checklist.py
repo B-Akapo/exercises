4aaee4cb3b2aa4e151f52d769cddf9df75b7ed00
Oluwabunmi Akapo <107144014+B-Akapo@users.noreply.github.com>
2023-08-11 13:29:13 +0100

    added docstring

--- python-basic-1/19-checklist.py
+++ python-basic-1/19-checklist.py
@@ -1,5 +1,15 @@
 # Description: A program that checks whether a specified value is contained within a group of values
 
+"""
+A program that checks whether a specified value is contained within a group of values.
+
+This program takes user input, validates it, and then checks if the input is in a predefined list of values. The result is then printed.
+
+Example Usage:
+    Please enter a number: 5
+    True
+"""
+
 def main():
     user_input = get_valid_input()
     result = check_list(user_input)
--- python-basic-1/20-histogram.py
+++ python-basic-1/20-histogram.py
@@ -1,5 +1,13 @@
 # Description: A program that creates a histogram from a given list of integers
 
+"""
+The code snippet is a standalone program that creates a histogram from a given list of integers. It starts by defining a `main` function, which is the entry point of the program. Inside the `main` function, a list of numbers `[2, 3, 6, 5]` is created and assigned to the variable `histogram_list`. Then, the `create_histogram` function is called with `histogram_list` as an argument.
+
+The `create_histogram` function takes a list of numbers as input and iterates over each number using a `for` loop. For each number, it prints a line of '#' characters equal to the value of the number. This creates a visual representation of the numbers as a histogram.
+
+Finally, the program checks if the `__name__` variable is equal to `"__main__"`, which indicates that the script is being run directly and not imported as a module. If this condition is true, the `main` function is called, executing the program.
+"""
+
 def main():
     histogram_list = [2, 3, 6, 5]
     create_histogram(histogram_list)
