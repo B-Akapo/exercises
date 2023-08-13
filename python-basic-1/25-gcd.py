8b2ce1aec88eb61928e6209d84fbaa902c11d618
Oluwabunmi Akapo <107144014+B-Akapo@users.noreply.github.com>
2023-08-13 22:13:23 +0100

    added validate_input function

--- python-basic-1/25-gcd.py
+++ python-basic-1/25-gcd.py
@@ -1,23 +1,24 @@
 # Description: A program that computes the greatest common divisor (GCD) of two positive integers. 
 
-"""
-This code snippet calculates the greatest common divisor (GCD) of two positive integers.
-
-
-Explanation:
-- The code snippet prompts the user to enter two positive integers and calculates their GCD using the math.gcd() function. 
-- The result is then printed.
-- There is an input validation to ensure that the user enters positive integers.
-"""
-
-import math 
+import math
 
 def main():
-    num1 = int(input("Enter the first positive integer: "))
-    num2 = int(input("Enter the second positive integer: "))
+    num1 = validate_input("Enter the first positive integer: ")
+    num2 = validate_input("Enter the second positive integer: ")
     
     gcd_result = calculate_gcd(num1, num2)
     print(f"The GCD of {num1} and {num2} is {gcd_result}")
+
+def validate_input(prompt):
+    while True:
+        try:
+            num = int(input(prompt))
+            if num <= 0:
+                print("Please enter a positive integer.")
+            else:
+                return num
+        except ValueError:
+            print("Please enter a valid integer.")
 
 def calculate_gcd(a, b):
     return math.gcd(a, b)
--- python-basic-1/26-lcm.py
+++ python-basic-1/26-lcm.py
@@ -3,11 +3,22 @@
 import math 
 
 def main():
-    num1 = int(input("Enter the first positive integer: "))
-    num2 = int(input("Enter the second positive integer: "))
+    num1 = validate_input("Enter the first positive integer: ")
+    num2 = validate_input("Enter the first positive integer: ")
     
     lcm_result = calculate_lcm(num1, num2)
     print(f"The LCM of {num1} and {num2} is {lcm_result}")
+
+def validate_input(prompt):
+    while True:
+        try:
+            num = int(input(prompt))
+            if num <= 0:
+                print("Please enter a positive integer.")
+            else:
+                return num
+        except ValueError:
+            print("Please enter a valid integer.")
 
 def calculate_lcm(a, b):
     return math.lcm(a, b)
