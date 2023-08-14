df3aaa09c0a1479958315d3e42b0a026d591a38f
Oluwabunmi Akapo <107144014+B-Akapo@users.noreply.github.com>
2023-08-14 15:41:03 +0100

    created 31-print.py

---
+++ python-basic-1/31-print.py
@@ -0,0 +1,16 @@
+# Description: that displays your name, age, and address on three different lines. 
+
+def main():
+    print_info()
+
+def print_info():
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
+    address = input("Enter your address: ")
+    
+    info = f"{name}\n{age}\n{address}"
+    print(info)
+
+if __name__ == "__main__":
+    main()
+
