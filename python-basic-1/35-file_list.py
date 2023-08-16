77c64d0341e5f662d730166ade6da0415d07fad1
Bunmi Akapo <107144014+B-Akapo@users.noreply.github.com>
2023-08-16 13:21:51 +0100

    create 35-file_list.py

---
+++ python-basic-1/35-file_list.py
@@ -0,0 +1,20 @@
+# Description: A program that checks if a file exists
+
+import os.path
+
+def main():
+    user_input = input("Please enter file name: ")  # Move this line inside the main function
+    if check_file(user_input):
+        print(f"{user_input} exists")
+    else:
+        print(f"{user_input} does not exist")
+
+def check_file(user_input):
+    if os.path.isfile(user_input):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    main()
+
